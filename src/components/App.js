ac714a25da0f4f815b3ab043d41c514a00186846
ACER <mahanasty@mail.ru>
2020-11-23 15:01:13 +0300

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export class App extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>Top comment</h1>
+            <div>
+                <img src="" alt=""/>
+                <p>Title</p>
+                <p>Number of comments :123</p>
+                <a href="#" target="_blank">Link</a>
+            </div>
+        </div>);
+  }
+}
