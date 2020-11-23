434b2cc509de9b0aab965900968d156b0db9c2d2
ACER <mahanasty@mail.ru>
2020-11-23 18:49:29 +0300

    на кнопку переключение autoRefresh, запрос каждые 3 секунды

--- src/components/App.js
+++ src/components/App.js
@@ -10,16 +10,21 @@ export class App extends React.Component {
         this.state={
             items:[],
             isLoader:false,
+            enableAutoRefresh:false,
         };
     }
     //https://www.reddit.com/r/reactjs.json?limit=100
     componentDidMount() {
+        this.getItems();
+    }
+
+    getItems=()=>{
         this.setState({
             isLoading:true,
         })
         fetch('https://www.reddit.com/r/reactjs.json?limit=100')
             .then(response =>{
-               return  response.json();
+                return  response.json();
             })
             .then(({ data })=>{
                 this.setState({
@@ -27,15 +32,51 @@ export class App extends React.Component {
                     isLoading:false,
                 })
             });
+    };
+
+    updateAutoRefresh = () => {
+        // if(this.state.enableAutoRefresh){
+        //     this.setState({
+        //             enableAutoRefresh:false,
+        //         });
+        //     clearInterval(this.autoRefresh);
+        // }else{
+        //     this.setState({
+        //         enableAutoRefresh:true,
+        //     })
+        //     this.autoRefresh=setInterval(this.getItems,3000);
+        // }
+
+        // Второй вариант , легче читаемая логика
+
+        this.setState(
+            state => ({
+                enableAutoRefresh: !state.enableAutoRefresh
+            }),
+            () => {
+                if (this.state.enableAutoRefresh) {
+                    this.autoRefresh = setInterval(this.getItems, 3000);
+                } else {
+                    clearInterval(this.autoRefresh);
+                }
+            }
+        );
     }
 
     render() {
-        const {items, isLoading} = this.state;
+        const { items, isLoading,enableAutoRefresh} = this.state;
+        const itemsSortByComments=items.sort(
+            (a,b)=>b.data.num_comments-a.data.num_comments
+        );
+
         return (
             <div className='container'>
                 <h1 className='header'>Top comment</h1>
+                <button type="button" onClick={this.updateAutoRefresh}>
+                    {enableAutoRefresh? "Stop":"Start"} auto-refresh</button>
+
                 {isLoading ? <div><img src={preloader} alt="preloader"/>LOADING...</div>
-                           : items.map(item => <Item key={item.data.id}
+                           : itemsSortByComments.map(item => <Item key={item.data.id}
                                          data={item.data}
                     />)
                 }
